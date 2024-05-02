afa5be630113c4149d88f1bbd9fae0c62b2b8fdc
dimensions27 <carlosjlopezz27@gmail.com>
2024-05-01 17:01:16 -0700

    updated

--- orders.php
+++ orders.php
@@ -29,21 +29,29 @@ session_start()
     </header>
 
     <div id="main-content">
-        <h2>Choose Your Drink</h2>
-        <form method="post" action="order_add.php">
-            <input type="radio" name="drink" value="caramel macchiato"> Caramel Macchiato<br>
-            <input type="radio" name="drink" value="iced vanilla latte"> Iced Vanilla Latte<br>
-            <input type="radio" name="drink" value="iced white chocolate mocha"> Iced White Chocolate Mocha<br>
-            <input type="radio" name="drink" value="raspberry cappuccino"> Raspberry Cappuccino<br>
-            <input type="radio" name="drink" value="custom"> Custom Drink<br>
-            <?php if ($_POST['drink'] === 'custom'): ?>
-            <div>
-                <label for="customDrink">Enter Custom Drink:</label>
-                <input type="text" name="customDrink" id="customDrink">
-            </div>
-            <?php endif; ?>
-            <input type="submit" value="Place Order">
-        </form>
+    <h2>Choose Your Drink</h2>
+    <form method="post" action="order_add.php" id="orderForm">
+        <input type="radio" name="drink" value="caramel macchiato"> Caramel Macchiato<br>
+        <input type="radio" name="drink" value="iced vanilla latte"> Iced Vanilla Latte<br>
+        <input type="radio" name="drink" value="iced white chocolate mocha"> Iced White Chocolate Mocha<br>
+        <input type="radio" name="drink" value="raspberry cappuccino"> Raspberry Cappuccino<br>
+        <input type="radio" name="drink" value="custom"> Custom Drink<br>
+        <div id="customDrinkInput" style="display: none;">
+            <label for="customDrink">Enter Custom Drink:</label>
+            <input type="text" name="customDrink" id="customDrink">
+        </div>
+        <input type="submit" value="Place Order">
+    </form>
+
+    <script>
+        document.querySelector('input[name="drink"]').addEventListener('change', function() {
+            if (this.value === 'custom') {
+                document.getElementById('customDrinkInput').style.display = 'block';
+            } else {
+                document.getElementById('customDrinkInput').style.display = 'none';
+            }
+        });
+    </script>
         
         <h2>Current Order</h2>
         <table class="table table-dark">
